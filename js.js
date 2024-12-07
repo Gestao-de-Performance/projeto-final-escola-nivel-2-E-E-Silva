226ff8bb61d49f5e55c743e94e4704f86643dd46
EmersonSS <130619133+E-E-Silva@users.noreply.github.com>
2024-12-07 18:20:24 -0300

    START
    Ó inicio

---
+++ js.js
@@ -0,0 +1,18 @@
+var menu = document.getElementById("menuPodrao");
+    
+function chamaMenu(){
+    if(menu.style.display == "none"){
+        menu.style.display = "block";
+    } else {
+    menu.style.display = "none";
+    }
+    
+}
+
+window.addEventListener('scroll', () => {
+    const moto = document.getElementById('Motos');
+    const scroll = window.scrollY;
+    moto.style.backgroundPosition = `${scrollY * 0.2}px top`;
+
+})
+
