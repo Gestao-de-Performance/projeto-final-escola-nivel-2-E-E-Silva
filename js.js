0a93de62c82dc34c6bff19ee8942d101960071fa
EmersonSS <130619133+E-E-Silva@users.noreply.github.com>
2024-12-08 09:52:51 -0300

    Finalizei
    Ficou bem básico mas é oque eu tenho capacidade de fazer no momento.

--- js.js
+++ js.js
@@ -16,3 +16,17 @@ window.addEventListener('scroll', () => {
 
 })
 
+var introducao = document.querySelector('.prologoPagina')
+
+function mudandoBackground() {
+    introducao.style.background = '#ffc894'
+    var textoTroca = document.getElementById('texto')
+    textoTroca.innerHTML = 'Bem-vindo ao universo das motos, onde a paixão pelo motociclismo encontra o conhecimento! No Moto Mania, não oferecemos apenas produtos; oferecemos uma imersão completa no mundo das duas rodas. Aqui, você encontra guias aprofundados sobre mecânica, dicas para personalização, curiosidades históricas e análises detalhadas das melhores motos do mercado. Descubra como escolher o motor ideal para o seu estilo, domine técnicas avançadas de pilotagem e explore o espírito de liberdade que só uma moto pode proporcionar. Prepare-se para acelerar no caminho do conhecimento e da adrenalina!'
+}
+
+function returBackground() {
+    introducao.style.background = 'white'
+    var returnadoTexto = document.getElementById('texto')
+    returnadoTexto.innerHTML = returnadoTexto.getAttribute('original')
+}
+
